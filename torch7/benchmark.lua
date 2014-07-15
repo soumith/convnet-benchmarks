9fff437a08e2bce685dfd3849395c16cdfb749ae
Soumith Chintala <soumith@gmail.com>
2014-07-15 11:17:49 -0400

    using clement's benchmark script instead

--- torch7/benchmark.lua
+++ torch7/benchmark.lua
@@ -1,45 +1,94 @@
 require 'nn'
-require 'xlua'
-require 'pl'
+require 'sys'
+require 'cunn'
  
- 
-opt = lapp[[
-   -t,--threads            (default 6)           number of threads
-   -p,--type               (default float)       float or cuda
-   -i,--devid              (default 1)           device ID (if using CUDA)
-]]
- 
-p = xlua.Profiler()
-torch.setnumthreads(opt.threads)
 torch.manualSeed(1)
 torch.setdefaulttensortype('torch.FloatTensor')
  
-if opt.type == 'cuda' then
-   require 'cunn'
-   cutorch.setDevice(opt.devid)
-   print('==> using GPU #' .. cutorch.getDevice())
-end
  
-batchSize = 128
-iH = 128
-iW = 128
-fin = 3
-fout = 96
-kH = 11
-kW = 11
+steps = 4 -- nb of steps in loop to average perf
+ops = 2 -- ops per point
 
-if opt.type == 'cuda' then   
-   input = torch.CudaTensor(fin,iH,iW,batchSize)
-   model = nn.SpatialConvolutionCUDA(fin, fout, kH, kW, 1, 1):cuda()
-else
-   input = torch.FloatTensor(batchSize,fin,iH,iW) 
-   model = nn.SpatialConvolution(fin, fout, kH, kW)   
+runs = {
+   {
+      ni = 3,
+      no = 96,
+      kw = 11,
+      kh = 11,
+      iw = 128,
+      ih = 128,
+      bs = 128,
+      dw = 1,
+      dh = 1,
+   },
+   {
+      ni = 64,
+      no = 128,
+      kw = 9,
+      kh = 9,
+      iw = 64,
+      ih = 64,
+      bs = 128,
+      dw = 1,
+      dh = 1,
+   },
+   {
+      ni = 128,
+      no = 128,
+      kw = 9,
+      kh = 9,
+      iw = 32,
+      ih = 32,
+      bs = 128,
+      dw = 1,
+      dh = 1,
+   },
+   {
+      ni = 128,
+      no = 128,
+      kw = 7,
+      kh = 7,
+      iw = 16,
+      ih = 16,
+      bs = 128,
+      dw = 1,
+      dh = 1,
+   },
+}
+
+for i,run in ipairs(runs) do
+   -- params for run:
+   local ni,no,kw,kh,bs,iw,ih,dw,dh = run.ni,run.no,run.kw,run.kh,run.bs,run.iw,run.ih,run.dw,run.dh
+   print('')
+   print('CONFIG: input = ' .. ni..'x'..iw..'x'..ih..' * ker = ' .. ni..'x'..no..'x'..kw..'x'..kh .. ' (bs = '..bs..', stride = ' .. dw .. ')')
+
+   n1 = nn.SpatialConvolutionCUDA(ni,no,kw,kh,dw,dh):cuda()
+   n2 = nn.SpatialConvolutionMM(ni,no,kw,kh,dw,dh):cuda()
+
+   i1 = torch.randn(ni, ih, iw, bs):cuda()
+   i2 = torch.randn(bs, ni, ih, iw):cuda()
+
+   o1 = n1:forward(i1)
+   o2 = n2:forward(i2)
+
+   cutorch.synchronize()
+   sys.tic()
+   for t = 1,steps do
+      o1 = n1:updateOutput(i1)
+   end
+   cutorch.synchronize()
+   tm = sys.toc()/steps
+   print('nn.SpatialConvolutionCUDA: ' .. (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) /dw/dh * bs * ops / tm / 1e9) .. ' GFLOP/s (tm = ' .. tm .. ')')
+
+   cutorch.synchronize()
+   sys.tic()
+   for t = 1,steps do
+      o2 = n2:updateOutput(i2)
+   end
+   cutorch.synchronize()
+   tm = sys.toc()/steps
+   print('nn.SpatialConvolutionMM: ' .. (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) /dw/dh * bs * ops / tm / 1e9) .. ' GFLOP/s (tm = ' .. tm .. ')')
+
+   collectgarbage()
 end
-p:start('spatialconv')
-output = model:forward(input)
-if opt.type == 'cuda' then cutorch.synchronize() end
-p:lap('spatialconv')
-p:printAll{}
- 
- 
-print('Gops/s:', ( batchSize*fin*fout*kH*kW*((iH-kH)+1)*((iW-kW)+1)*2 ) / p:cpu('spatialconv') / 1e9 ) -- 2 operations MUL, ACC
+
