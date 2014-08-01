1008512642403bf081371917506b4d31510353cf
Jan Schlüter <jan.schlueter@ofai.at>
2014-08-01 14:16:56 +0200

    Added backpropagation benchmarks for Theano

--- theano/pylearn2_benchmark.py
+++ theano/pylearn2_benchmark.py
@@ -10,6 +10,7 @@ from pylearn2.models.mlp import ConvElemwise, ConvNonlinearity, MLP
 from pylearn2.space import Conv2DSpace
 
 from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
+from pylearn2.sandbox.cuda_convnet.weight_acts import WeightActs
 from theano.sandbox.cuda.basic_ops import gpu_contiguous
 
 
@@ -90,11 +91,13 @@ for i in range(5):
     inputBatch = np.random.randn(bs, ni, ih, iw)
     sharedX = theano.shared(inputBatch.astype('float32'))
     sharedY = theano.shared(np.random.randn(bs, no, (ih-kh)/dh+1, (iw-kw)/dw+1).astype('float32'))
+    sharedW = conv.layers[0].transformer.get_params()[0]
 
     X = theano.tensor.tensor4()
-
     Y = conv.fprop(X)
+    gW = theano.grad(None, wrt=sharedW, known_grads={Y: sharedY})
 
+    # benchmark fprop Theano standard convolution
     fprop = theano.function([], [], givens=[(X, sharedX)], updates=[(sharedY, Y)], on_unused_input='ignore')
 
     theano.sandbox.cuda.synchronize()
@@ -104,11 +107,28 @@ for i in range(5):
     theano.sandbox.cuda.synchronize()
     tm = (time.time()-start)/steps
 
-    print 'pylearn2.models.mlp.ConvElemwise:', (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) / dw/dh * bs * ops / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'
+    del fprop
+    print 'pylearn2.models.mlp.ConvElemwise fprop:', (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) / dw/dh * bs * ops / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'
+
+    # benchmark bprop Theano standard convolution
+    bprop = theano.function([], [], givens=[(X, sharedX)], updates=[(sharedW, gW)], on_unused_input='ignore')
+
+    theano.sandbox.cuda.synchronize()
+    start = time.time()
+    for i in range(steps):
+        bprop()
+    theano.sandbox.cuda.synchronize()
+    tm = (time.time()-start)/steps
+
+    del bprop
+    print 'pylearn2.models.mlp.ConvElemwise bprop:', (0000 / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'  # TODO: GFLOP/s
+
 
     # Mimic Theano flag THEANO_FLAGS=optimizer_including=conv_fft_valid:conv_fft_full
     mode = theano.compile.get_default_mode()
     mode = mode.including('conv_fft_valid', 'conv_fft_full')
+
+    # benchmark fprop Theano FFT convolution
     fprop = theano.function([], [], givens=[(X, sharedX)],
                             updates=[(sharedY, Y)],
                             on_unused_input='ignore', mode=mode)
@@ -121,11 +141,28 @@ for i in range(5):
     tm = (time.time()-start)/steps
 
     del fprop
-    del sharedX
-    del conv
-    del sharedY
+    print '(fft experimental) pylearn2.models.mlp.ConvElemwise fprop:', (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) / dw/dh * bs * ops / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'
 
-    print '(fft experimental) pylearn2.models.mlp.ConvElemwise:', (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) / dw/dh * bs * ops / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'
+    # benchmark bprop Theano FFT convolution
+    bprop = theano.function([], [], givens=[(X, sharedX)],
+                            updates=[(sharedW, gW)],
+                            on_unused_input='ignore', mode=mode)
+
+    theano.sandbox.cuda.synchronize()
+    start = time.time()
+    for i in range(steps):
+        bprop()
+    theano.sandbox.cuda.synchronize()
+    tm = (time.time()-start)/steps
+
+    del bprop
+    print '(fft experimental) pylearn2.models.mlp.ConvElemwise bprop:', (0000 / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'  # TODO: GFLOP/s
+
+    del sharedX
+    del sharedY
+    del sharedW
+    del conv
+
 
     ### pylearn2 work-around for using cuda-convnet (http://benanne.github.io/2014/04/03/faster-convolutions-in-theano.html) ###
 
@@ -140,7 +177,10 @@ for i in range(5):
     contiguous_input = gpu_contiguous(sharedX)
     contiguous_filters = gpu_contiguous(sharedW)
     Y = conv_op(contiguous_input, contiguous_filters)
+    gW = theano.grad(None, wrt=sharedW, known_grads={Y: sharedY})
+    #gW = WeightActs()(contiguous_input, gpu_contiguous(sharedY), sharedW.shape)[0]  # constructed by hand, results in the same graph
 
+    # benchmark fprop cuda-convnet convolution
     fprop = theano.function([], [], givens=[(X, sharedX)], updates=[(sharedY, Y)], on_unused_input='ignore')
 
     theano.sandbox.cuda.synchronize()
@@ -151,9 +191,23 @@ for i in range(5):
     tm = (time.time()-start)/steps
 
     del fprop
+    print 'pylearn2.sandbox.cuda_convnet fprop:', (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) / dw/dh * bs * ops / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'
+
+    # benchmark bprop cuda-convnet convolution
+    bprop = theano.function([], [], givens=[(X, sharedX)], updates=[(sharedW, gW)], on_unused_input='ignore')
+
+    theano.sandbox.cuda.synchronize()
+    start = time.time()
+    for i in range(steps):
+        bprop()
+    theano.sandbox.cuda.synchronize()
+    tm = (time.time()-start)/steps
+
+    del bprop
+    print 'pylearn2.sandbox.cuda_convnet bprop:', (0000 / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'  # TODO: GFLOP/s
+
     del sharedX
-    del conv_op
     del sharedY
     del sharedW
+    del conv_op
 
-    print ' pylearn2.sandbox.cuda_convnet:', (ni*no*kw*kh*(iw-kw+1)*(ih-kh+1) / dw/dh * bs * ops / tm / 1e9), 'GFLOP/s ( tm =', tm, ')'
