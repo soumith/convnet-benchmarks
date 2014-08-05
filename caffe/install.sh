a46e46c44272f9cc5e85ac87c2380e8ad9c4275b
Soumith Chintala <soumith@gmail.com>
2014-08-05 11:57:23 -0400

    simplified caffe install.sh

--- caffe/install.sh
+++ caffe/install.sh
@@ -3,18 +3,12 @@ cd caffe
 git checkout dev
 
 # Dependencies
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev libgflags-dev
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler
+sudo apt-get install gcc-4.6 g++-4.6
 
-wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
-tar zxvf glog-0.3.3.tar.gz
-cd glog-0.3.3
-./configure
-make && sudo make install
-cd ..
-
-sudo apt-get install liblmdb-dev
-
+export CC=gcc-4.6
+export CXX=g++-4.6
 # Compile Caffe
 cp Makefile.config.example Makefile.config
 # Adjust Makefile.config (for example, if using Anaconda Python)
