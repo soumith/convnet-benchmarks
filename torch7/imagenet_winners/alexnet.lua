a553ec50f5c9c7159482f656bdb9743eb53d1044
soumith <soumith@fb.com>
2014-12-17 17:17:58 -0800

    modifying alexnet to OWT version

--- torch7/imagenet_winners/alexnet.lua
+++ torch7/imagenet_winners/alexnet.lua
@@ -3,25 +3,23 @@ function alexnet(lib)
    local SpatialMaxPooling = lib[2]
    local ReLU = lib[3]
 
-   local features = nn.Concat(2)
-   local branch1 = nn.Sequential() -- branch 1
-   branch1:add(SpatialConvolution(3,48,11,11,4,4,2,2))       -- 224 -> 55
-   branch1:add(ReLU())
-   branch1:add(SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
-   branch1:add(SpatialConvolution(48,128,5,5,1,1,2,2))       --  27 -> 27
-   branch1:add(ReLU())
-   branch1:add(SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
-   branch1:add(SpatialConvolution(128,192,3,3,1,1,1,1))      --  13 ->  13
-   branch1:add(ReLU())
-   branch1:add(SpatialConvolution(192,192,3,3,1,1,1,1))      --  13 ->  13
-   branch1:add(ReLU())
-   branch1:add(SpatialConvolution(192,128,3,3,1,1,1,1))      --  13 ->  13
-   branch1:add(ReLU())
-   branch1:add(SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
+   -- from https://code.google.com/p/cuda-convnet2/source/browse/layers/layers-imagenet-1gpu.cfg
+   -- this is AlexNet that was presented in the One Weird Trick paper. http://arxiv.org/abs/1404.5997
+   local features = nn.Sequential()
+   features:add(SpatialConvolution(3,64,11,11,4,4,2,2))       -- 224 -> 55
+   features:add(ReLU())
+   features:add(SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
+   features:add(SpatialConvolution(64,192,5,5,1,1,2,2))       --  27 -> 27
+   features:add(ReLU())
+   features:add(SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
+   features:add(SpatialConvolution(192,384,3,3,1,1,1,1))      --  13 ->  13
+   features:add(ReLU())
+   features:add(SpatialConvolution(384,256,3,3,1,1,1,1))      --  13 ->  13
+   features:add(ReLU())
+   features:add(SpatialConvolution(256,256,3,3,1,1,1,1))      --  13 ->  13
+   features:add(ReLU())
+   features:add(SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
 
-   local branch2 = branch1:clone() -- branch 2, if using in real-life, reset weights of this branch
-   features:add(branch1)
-   features:add(branch2)
    local classifier = nn.Sequential()
    classifier:add(nn.View(256*6*6))
    -- classifier:add(nn.Dropout(0.5))
@@ -35,7 +33,7 @@ function alexnet(lib)
 
    local model = nn.Sequential()
    model:add(features):add(classifier)
-   
+
    return model,'AlexNet',{128,3,224,224}
 end
 
