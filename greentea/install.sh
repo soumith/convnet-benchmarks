3bbf16500ade63694e16bbc609ca48909ad67f1e
Fabian Tschopp <fabian@m-tschopp.ch>
2015-08-09 03:21:22 +0200

    Updated Makefile and removed log files.

--- greentea/install.sh
+++ greentea/install.sh
@@ -6,12 +6,14 @@ git checkout master
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev 
 sudo apt-get install -y protobuf-compiler gfortran libjpeg62 libfreeimage-dev libatlas-base-dev git python-dev python-pip 
 sudo apt-get install -y libgoogle-glog-dev libbz2-dev libxml2-dev libxslt-dev libffi-dev libssl-dev libgflags-dev liblmdb-dev python-yaml
-sudo apt-get install -y libviennacl-dev opencl-headers
+sudo apt-get install -y libviennacl-dev opencl-headers libopenblas-base libopenblas-dev
 sudo easy_install pillow
 
 # Compile Caffe
 cp ../Makefile.config Makefile.config
 
-make all
-make test
-make runtest
+cores=`grep -c ^processor /proc/cpuinfo`
+
+make all -j$cores
+make test -j$cores
+make runtest -j$cores
