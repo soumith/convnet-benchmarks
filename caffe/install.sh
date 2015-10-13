7e75916f28ed1f3dac1dffd7e0b565cc032266a9
soumith <soumith@gmail.com>
2015-10-12 21:10:49 -0400

    caffe cpu

--- caffe/install.sh
+++ caffe/install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/BVLC/caffe.git
 cd caffe
-git checkout dev
+
 
 # Dependencies
 sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev 
@@ -8,10 +8,14 @@ sudo apt-get install -y protobuf-compiler gfortran libjpeg62 libfreeimage-dev li
 sudo apt-get install -y libgoogle-glog-dev libbz2-dev libxml2-dev libxslt-dev libffi-dev libssl-dev libgflags-dev liblmdb-dev python-yaml
 sudo easy_install pillow
 
-# Compile Caffe
-cp Makefile.config.example Makefile.config
-# For some reason, I was getting <mpi.h> not found. So I had to manually edit /usr/include/H5public.h and disable PARALLEL support. (by adding #undef H5_HAVE_PARALLEL )
-# Also, I disabled CUDNN (because these are pure-caffe benchmarks)
-make all
-make test
-make runtest
+mkdir build
+cd build
+export CMAKE_LIBRARY_PATH=/opt/OpenBLAS/include:/opt/OpenBLAS/lib:$CMAKE_LIBRARY_PATH
+cmake .. -DCPU_ONLY=1 -DBLAS="open"
+make
+cd ../../
+
+#wget -c https://software.intel.com/sites/default/files/managed/ad/a4/intel_optimized_technical_preview_for_multinode_caffe_1.0.tgz
+#tar -xvf intel_optimized_technical_preview_for_multinode_caffe_1.0
+#cd intel_optimized_technical_preview_for_multinode_caffe_1.0
+#unzip -P accept intel_optimized_technical_preview_for_multinode_caffe_1.0.zip
