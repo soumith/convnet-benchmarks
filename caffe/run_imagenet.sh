cbfd8e09bcc21110fe28e196ee42e4990aa3c81a
soumith <soumith@gmail.com>
2015-10-22 01:19:44 -0400

    fixing caffe+mkl numbers

--- caffe/run_imagenet.sh
+++ caffe/run_imagenet.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-./caffe/build/tools/caffe time --model=./imagenet_winners/alexnet.prototxt --iterations=10 --logtostderr=1 >>output_alexnet.log 2>&1
-./caffe/build/tools/caffe time --model=./imagenet_winners/overfeat.prototxt --iterations=10 --logtostderr=1 >>output_overfeat.log 2>&1
-./caffe/build/tools/caffe time --model=./imagenet_winners/vgg_a.prototxt --iterations=10 --logtostderr=1 >>output_vgg_a.log 2>&1
-./caffe/build/tools/caffe time --model=./imagenet_winners/googlenet.prototxt --iterations=10 --logtostderr=1 >>output_googlenet.log 2>&1
+#./caffe/build/tools/caffe time --model=./imagenet_winners/alexnet.prototxt --iterations=10 --logtostderr=1 >>output_alexnet.log 2>&1
+#./caffe/build/tools/caffe time --model=./imagenet_winners/overfeat.prototxt --iterations=10 --logtostderr=1 >>output_overfeat.log 2>&1
+#./caffe/build/tools/caffe time --model=./imagenet_winners/vgg_a.prototxt --iterations=10 --logtostderr=1 >>output_vgg_a.log 2>&1
+#./caffe/build/tools/caffe time --model=./imagenet_winners/googlenet.prototxt --iterations=10 --logtostderr=1 >>output_googlenet.log 2>&1
 
+./caffe/build/tools/caffe time --model=./alexnet.prototxt --iterations=10 --logtostderr=1 >output_alexnet_mkl.log 2>&1
