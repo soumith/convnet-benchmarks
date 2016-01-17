9708206dc6733ed1230f5916ad433efe0e72f35f
Marat Dukhan <maratek@gmail.com>
2016-01-17 15:51:15 -0500

    Build/bench Caffe with both MKL and OpenBLAS

--- caffe/run_imagenet.sh
+++ caffe/run_imagenet.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#./caffe/build/tools/caffe time --model=./imagenet_winners/alexnet.prototxt --iterations=10 --logtostderr=1 >>output_alexnet.log 2>&1
-#./caffe/build/tools/caffe time --model=./imagenet_winners/overfeat.prototxt --iterations=10 --logtostderr=1 >>output_overfeat.log 2>&1
-#./caffe/build/tools/caffe time --model=./imagenet_winners/vgg_a.prototxt --iterations=10 --logtostderr=1 >>output_vgg_a.log 2>&1
-#./caffe/build/tools/caffe time --model=./imagenet_winners/googlenet.prototxt --iterations=10 --logtostderr=1 >>output_googlenet.log 2>&1
+for BLAS in mkl openblas
+do
+	./caffe/build-with-${BLAS}/tools/caffe time --model=./imagenet_winners/alexnet.prototxt --iterations=10 --logtostderr=1 >>output_alexnet_${BLAS}.log 2>&1
+	./caffe/build-with-${BLAS}/tools/caffe time --model=./imagenet_winners/overfeat.prototxt --iterations=10 --logtostderr=1 >>output_overfeat_${BLAS}.log 2>&1
+	./caffe/build-with-${BLAS}/tools/caffe time --model=./imagenet_winners/vgg_a.prototxt --iterations=10 --logtostderr=1 >>output_vgg_a_${BLAS}.log 2>&1
+	./caffe/build-with-${BLAS}/tools/caffe time --model=./imagenet_winners/googlenet.prototxt --iterations=10 --logtostderr=1 >>output_googlenet_${BLAS}.log 2>&1
+done
 
-./caffe/build/tools/caffe time --model=./alexnet.prototxt --iterations=10 --logtostderr=1 >output_alexnet_mkl.log 2>&1
