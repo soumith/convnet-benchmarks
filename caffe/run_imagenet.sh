e5b187d607ea592ed33e7ca2eaf4c6175e271328
Marat Dukhan <maratek@gmail.com>
2016-01-17 16:45:58 -0500

    Explicitly specify number of threads in Caffe/CPU benchmarks

--- caffe/run_imagenet.sh
+++ caffe/run_imagenet.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+NUM_THREADS=`cat /proc/cpuinfo | grep 'processor' | wc -l`
+echo "Running with ${NUM_THREADS} threads"
+
+export OPENBLAS_NUM_THREADS=${NUM_THREADS}
+export MKL_NUM_THREADS=${NUM_THREADS}
+export MKL_BLAS_NUM_THREADS=${NUM_THREADS}
+export OMP_NUM_THREADS=${NUM_THREADS}
+
+
 for BLAS in mkl openblas
 do
 	./caffe/build-with-${BLAS}/tools/caffe time --model=./imagenet_winners/alexnet.prototxt --iterations=10 --logtostderr=1 >>output_alexnet_${BLAS}.log 2>&1
