924ab71f8f00d3b5ad556d588447c513af39e37b
Shunta Saito <shunta.saito@gmail.com>
2017-06-09 23:59:04 +0900

    Add Chainer V2

--- chainer/alex.py
+++ chainer/alex.py
@@ -1,26 +1,24 @@
 import chainer
 import chainer.functions as F
+import chainer.links as L
 
 
-class Alex(chainer.FunctionSet):
+class Alex(chainer.Chain):
     insize = 224
 
     def __init__(self):
-        super(Alex, self).__init__(
-            conv1=F.Convolution2D(3,  64, 11, stride=4, pad=2),
-            conv2=F.Convolution2D(64, 192,  5, pad=2),
-            conv3=F.Convolution2D(192, 384,  3, pad=1),
-            conv4=F.Convolution2D(384, 256,  3, pad=1),
-            conv5=F.Convolution2D(256, 256,  3, pad=1),
-            fc6=F.Linear(256 * 6 * 6, 4096),
-            fc7=F.Linear(4096, 4096),
-            fc8=F.Linear(4096, 1000),
-        )
+        super(Alex, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(3,  64, 11, stride=4, pad=2)
+            self.conv2 = L.Convolution2D(64, 192,  5, pad=2)
+            self.conv3 = L.Convolution2D(192, 384,  3, pad=1)
+            self.conv4 = L.Convolution2D(384, 256,  3, pad=1)
+            self.conv5 = L.Convolution2D(256, 256,  3, pad=1)
+            self.fc6 = L.Linear(256 * 6 * 6, 4096)
+            self.fc7 = L.Linear(4096, 4096)
+            self.fc8 = L.Linear(4096, 1000)
 
-    def forward(self, x_data, y_data, train=True):
-        x = chainer.Variable(x_data, volatile=False)
-        t = chainer.Variable(y_data, volatile=False)
-
+    def forward(self, x):
         h = F.max_pooling_2d(F.relu(self.conv1(x)), 3, stride=2)
         h = F.max_pooling_2d(F.relu(self.conv2(h)), 3, stride=2)
         h = F.relu(self.conv3(h))
@@ -28,5 +26,4 @@ class Alex(chainer.FunctionSet):
         h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
         h = F.relu(self.fc6(h))
         h = F.relu(self.fc7(h))
-        h = self.fc8(h)
-        return F.softmax_cross_entropy(h, t), F.accuracy(h, t)
+        return self.fc8(h)
